7a2b4d8c4c1c288894cea0a1b568f2f1e54a059b
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-12-11 12:42:25 -0300

    criando service de livro

--- src/livro/livro.module.ts
+++ src/livro/livro.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LivroController } from './livro.controller';
+import { LivroService } from './livro.service';
 
 @Module({
-  controllers: [LivroController]
+  controllers: [LivroController],
+  providers: [LivroService]
 })
 export class LivroModule {}
