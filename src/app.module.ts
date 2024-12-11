001b13b491b01c9ad24195b1e341c451017c7f42
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-12-11 14:41:20 -0300

    adicionando modulos de usuarios, livro e avalaliação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
-import { LivroModule } from './livro/livro.module';
+import { LivrosModule } from './livro/livro.module';
 import { AvaliacaoModule } from './avaliacao/avaliacao.module';
 
 @Module({
-  imports: [PrismaModule, UsuariosModule, LivroModule, AvaliacaoModule],
+  imports: [PrismaModule, UsuariosModule, LivrosModule, AvaliacaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
