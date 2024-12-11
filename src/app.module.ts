a3202e4eabc1423b2bd722973469858a204a7c9b
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-12-11 12:40:12 -0300

    importando modulos de livros e avaliação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { LivroModule } from './livro/livro.module';
+import { AvaliacaoModule } from './avaliacao/avaliacao.module';
 
 @Module({
-  imports: [PrismaModule, UsuariosModule],
+  imports: [PrismaModule, UsuariosModule, LivroModule, AvaliacaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
