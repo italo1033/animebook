020c2f3a70e429bd8f5b1eaf8f415e39fea5ef70
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-12-11 14:42:54 -0300

    adicionando module avaliacao

--- src/avaliacao/avaliacao.module.ts
+++ src/avaliacao/avaliacao.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AvaliacaoController } from './avaliacao.controller';
-import { AvaliacaoService } from './avaliacao.service';
+import { AvaliacoesService } from './avaliacao.service';
+import { AvaliacoesController } from './avaliacao.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  controllers: [AvaliacaoController],
-  providers: [AvaliacaoService]
+  imports: [PrismaModule], // Importa o PrismaModule
+  controllers: [AvaliacoesController],
+  providers: [AvaliacoesService],
 })
 export class AvaliacaoModule {}
