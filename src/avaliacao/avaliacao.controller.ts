db2127a45a2e177ced7087772cba25e7d4170cb9
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-12-12 10:44:15 -0300

    adicionando controlador de avaliação para mongo db

--- src/avaliacao/avaliacao.controller.ts
+++ src/avaliacao/avaliacao.controller.ts
@@ -1,46 +1,25 @@
-import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { AvaliacoesService } from './avaliacao.service';
-import { ApiTags, ApiOperation, ApiBody, ApiQuery, ApiParam } from '@nestjs/swagger';
-import { Avaliacao } from '@prisma/client';
+import { Avaliacao } from './schemas/avaliacao.schema';
 
 @Controller('avaliacoes')
-@ApiTags('avaliacoes')
 export class AvaliacoesController {
   constructor(private readonly avaliacoesService: AvaliacoesService) {}
 
-  // Criar uma avaliação
   @Post()
-  @ApiOperation({ summary: 'Cria uma avaliação para um livro' })
-  @ApiBody({
-    description: 'Dados da avaliação',
-    schema: {
-      example: {
-        usuarioId: 1,
-        livroId: 1,
-        nota: 5,
-        comentario: 'Excelente livro!',
-      },
-    },
-  })
   async criarAvaliacao(
-    @Body() body: { usuarioId: number; livroId: number; nota: number; comentario: string },
+    @Body() body: { usuarioId: string; livroId: string; nota: number; comentario: string },
   ): Promise<Avaliacao> {
-    return this.avaliacoesService.criarAvaliacao(body.usuarioId, body.livroId, body.nota, body.comentario);
+    return this.avaliacoesService.criarAvaliacao(body);
   }
 
-  // Listar todas as avaliações de um livro
-  @Get('livro/:livroId')
-  @ApiOperation({ summary: 'Lista todas as avaliações de um livro' })
-  @ApiParam({ name: 'livroId', description: 'ID do livro' })
-  async listarAvaliacoesPorLivro(@Param('livroId') livroId: number): Promise<Avaliacao[]> {
-    return this.avaliacoesService.listarAvaliacoesPorLivro(livroId);
+  @Get('usuario')
+  async buscarAvaliacoesPorUsuario(@Query('usuarioId') usuarioId: string): Promise<Avaliacao[]> {
+    return this.avaliacoesService.buscarAvaliacoesPorUsuario(usuarioId);
   }
 
-  // Listar todas as avaliações de um usuário
-  @Get('usuario/:usuarioId')
-  @ApiOperation({ summary: 'Lista todas as avaliações de um usuário' })
-  @ApiParam({ name: 'usuarioId', description: 'ID do usuário' })
-  async listarAvaliacoesPorUsuario(@Param('usuarioId') usuarioId: number): Promise<Avaliacao[]> {
-    return this.avaliacoesService.listarAvaliacoesPorUsuario(usuarioId);
+  @Get()
+  async listarAvaliacoes(): Promise<Avaliacao[]> {
+    return this.avaliacoesService.listarAvaliacoes();
   }
 }
