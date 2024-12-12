893c81f2cbf5b39973a3022be9966be86bc37689
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-12-12 10:44:44 -0300

    adicionando controlador de usuario para mongo db

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,37 +1,25 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
+import { Usuario } from './schemas/usuario.schema';
 
 @Controller('usuarios')
-@ApiTags('usuarios') // Tag para o Swagger
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
   @Post()
-  @ApiOperation({ summary: 'Cria um novo usuário' })
-  @ApiBody({
-    description: 'Dados do usuário a ser criado',
-    schema: {
-      example: {
-        nome: 'João da Silva',
-        email: 'joao.silva@example.com',
-        preferencias: ['Ficção', 'História', 'Ciência'],
-      },
-    },
-  })
   async criarUsuario(
-    @Body() body: { nome: string; email: string; preferencias: string },
-  ) {
-    return this.usuariosService.criarUsuario({
-      nome: body.nome,
-      email: body.email,
-      preferencias: body.preferencias,
-    });
+    @Body() body: { nome: string; email: string; preferencias: string[] },
+  ): Promise<Usuario> {
+    return this.usuariosService.criarUsuario(body); // Chamada com um único objeto
   }
 
   @Get()
-  @ApiOperation({ summary: 'Lista todos os usuários' })
-  async listarUsuarios() {
+  async listarUsuarios(): Promise<Usuario[]> {
     return this.usuariosService.listarUsuarios();
+  }
+
+  @Get(':id')
+  async buscarUsuario(@Param('id') id: string): Promise<Usuario> {
+    return this.usuariosService.buscarUsuarioPorId(id);
   }
 }
